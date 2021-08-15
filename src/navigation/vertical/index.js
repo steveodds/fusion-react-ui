0dd939327349243b0979d1851b07d9243116235d
Stephen Odundo <steveremdi@gmail.com>
2021-08-15 17:14:03 +0300

    Modified navigation menu to only show the necessary menu items

--- src/navigation/vertical/index.js
+++ src/navigation/vertical/index.js
@@ -1,12 +1,5 @@
 // ** Navigation sections imports
-import apps from './apps'
-import pages from './pages'
-import forms from './forms'
-import tables from './tables'
-import others from './others'
-import dashboards from './dashboards'
-import uiElements from './ui-elements'
-import chartsAndMaps from './charts-maps'
+import menu from './menu'
 
 // ** Merge & Export
-export default [...dashboards, ...apps, ...pages, ...uiElements, ...forms, ...tables, ...chartsAndMaps, ...others]
+export default [...menu]
