f85081dae7a771a482b3fc7c894560443a8cff72
Stephen Odundo <steveremdi@gmail.com>
2021-08-18 13:46:20 +0300

    updated import and added new license chart component

--- src/views/dashboard/ecommerce/index.js
+++ src/views/dashboard/ecommerce/index.js
@@ -16,7 +16,8 @@ import { getProjects } from '../../../fusionapis/get.projects'
 
 import '@styles/react/libs/charts/apex-charts.scss'
 import '@styles/base/pages/dashboard-ecommerce.scss'
-import ProjectTable from '../projects/ProjectTable'
+import ProjectTable from '../fusion_components/ProjectTable'
+import LicenseChart from '../fusion_components/LicenseChart'
 
 const EcommerceDashboard = (props) => {
   const { colors } = useContext(ThemeColors),
@@ -32,9 +33,9 @@ const EcommerceDashboard = (props) => {
         <Col xl='8' md='6' xs='12'>
           <StatsCard cols={{ xl: '3', sm: '6' }} />
         </Col>
-      </Row>
+      </Row> */}
       <Row className='match-height'>
-        <Col lg='4' md='12'>
+        {/* <Col lg='4' md='12'>
           <Row className='match-height'>
             <Col lg='6' md='3' xs='6'>
               <OrdersBarChart warning={colors.warning.main} />
@@ -46,11 +47,11 @@ const EcommerceDashboard = (props) => {
               <Earnings success={colors.success.main} />
             </Col>
           </Row>
+        </Col> */}
+        <Col>
+          <LicenseChart token={token} primary={colors.primary.main} warning={colors.warning.main} />
         </Col>
-        <Col lg='8' md='12'>
-          <RevenueReport primary={colors.primary.main} warning={colors.warning.main} />
-        </Col>
-      </Row> */}
+      </Row>
       <Row className='match-height'>
         <Col>
           <ProjectTable token={token} />
