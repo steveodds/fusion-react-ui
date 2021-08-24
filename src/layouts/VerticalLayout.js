7fd4b849cb7ec1ae26388e2d1ca774687e1e8281
Stephen Odundo <steveremdi@gmail.com>
2021-08-24 17:11:17 +0300

    props are no longer passed to custom navbar

--- src/layouts/VerticalLayout.js
+++ src/layouts/VerticalLayout.js
@@ -1,10 +1,10 @@
 // !Do not remove the Layout import
 import Layout from '@layouts/VerticalLayout'
 
-// import ThemeNavbar from '../views/components/CustomNavbar'
-import CustomNavbar from '../views/components/CustomNavbar/Navbar'
+import ThemeNavbar from '../views/components/CustomNavbar'
+// import CustomNavbar from '../views/components/CustomNavbar/Navbar'
 
 // const VerticalLayout = props => <Layout {...props}> {props.children} </Layout>
-const VerticalLayout = props => <Layout navbar={props => <CustomNavbar {...props} />} {...props}> {props.children} </Layout>
+const VerticalLayout = props => <Layout navbar={<ThemeNavbar />} {...props}> </Layout>
 
 export default VerticalLayout
