e600d3b7da055dbf587d30d9cc11791d38ef2f7c
Stephen Odundo <steveremdi@gmail.com>
2021-08-24 12:17:51 +0300

    updated global app name and logo

--- src/configs/themeConfig.js
+++ src/configs/themeConfig.js
@@ -3,8 +3,8 @@
 //Template config options
 const themeConfig = {
   app: {
-    appName: 'Vuexy',
-    appLogoImage: require('@src/assets/images/logo/logo.svg').default
+    appName: 'FusionAI',
+    appLogoImage: require('@src/assets/images/logo/infosight-logo-final-small.d9dff99c.png').default
   },
   layout: {
     isRTL: false,
@@ -24,7 +24,7 @@ const themeConfig = {
     footer: {
       type: 'static' // static, sticky, hidden
     },
-    customizer: true,
+    customizer: false,
     scrollTop: true // Enable scroll to top button
   }
 }
