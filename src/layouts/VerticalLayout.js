7ebb6a5c4bb4028013348113c0916f638a15a57d
Stephen Odundo <steveremdi@gmail.com>
2021-08-24 17:26:26 +0300

    Added custom footer

--- src/layouts/VerticalLayout.js
+++ src/layouts/VerticalLayout.js
@@ -2,9 +2,10 @@
 import Layout from '@layouts/VerticalLayout'
 
 import ThemeNavbar from '../views/components/CustomNavbar'
+import CustomFooter from '../views/components/CustomFooter/Footer'
 // import CustomNavbar from '../views/components/CustomNavbar/Navbar'
 
 // const VerticalLayout = props => <Layout {...props}> {props.children} </Layout>
-const VerticalLayout = props => <Layout navbar={<ThemeNavbar />} {...props}> </Layout>
+const VerticalLayout = props => <Layout navbar={<ThemeNavbar />} footer={<CustomFooter />} {...props}> </Layout>
 
 export default VerticalLayout
---
+++ src/views/components/CustomFooter/Footer.js
@@ -0,0 +1,5 @@
+const CustomFooter = props => {
+    return <h6>Copyright © 2021 Infosight Technologies, All Rights Reserved</h6>
+  }
+  
+  export default CustomFooter
