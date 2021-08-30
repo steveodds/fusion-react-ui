49a69bd0205002019dca354a8ea61002ac45dfc0
Stephen Odundo <steveremdi@gmail.com>
2021-08-30 09:54:04 +0300

    added new page for diplaying projects

---
+++ src/views/dashboard/projects/index.js
@@ -0,0 +1,26 @@
+import { useContext } from 'react'
+import { Row, Col } from 'reactstrap'
+
+import '@styles/react/libs/charts/apex-charts.scss'
+import '@styles/base/pages/dashboard-ecommerce.scss'
+import ProjectsList from './projectlist'
+import ProjectTable from '../fusion_components/ProjectTable'
+
+const Projects = () => {
+  let token = ""
+  if (document.cookie) {
+    token = document.cookie.split('; ').find(row => row.startsWith('fusion=')).split('=')[1]
+  }
+  return (
+    <div>
+      <Row className='match-height'>
+        <Col>
+          {/* <ProjectsList token={token} /> */}
+          <ProjectTable token={token} />
+        </Col>
+      </Row>
+    </div>
+  )
+}
+
+export default Projects
