10d6fcca884e4bbfdd03e32b11baba45d63ba1d0
Stephen Odundo <steveremdi@gmail.com>
2021-08-31 14:22:02 +0300

    added custom config for the default auth implementation

--- src/auth/jwt/useJwt.js
+++ src/auth/jwt/useJwt.js
@@ -1,6 +1,20 @@
 // ** Core JWT Import
 import useJwt from '@src/@core/auth/jwt/useJwt'
 
+const jwtConfig = {
+    loginEndpoint: 'https://trial.infosight.io/webapi/login',
+    registerEndpoint: '/jwt/register', //TODO Update uri
+    refreshEndpoint: '/jwt/refresh-token',
+    logoutEndpoint: '/jwt/logout',
+
+    // ** This will be prefixed in authorization header with token
+    // ? e.g. Authorization: Bearer <token>
+    tokenType: 'Bearer',
+
+    // ** Value of this property will be used as key to store JWT token in storage
+    storageTokenKeyName: 'accessToken',
+    storageRefreshTokenKeyName: 'refreshToken'
+}
 const { jwt } = useJwt({})
 
 export default jwt
